0a4563393d822300b71aeba2456b45bce0cc710e
Cecília <103593379+silvaceciliaa@users.noreply.github.com>
2022-08-12 20:58:54 -0300

    Add files via upload

---
+++ Maquina.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquinaDeBebidas
+{
+    public class Maquina
+    {
+        public string OpcaoMenu { get; set; }
+        public int Refrigerante { get; set; }
+
+        public int Suco { get; set; }
+
+        public double Cafe { get; set; }
+
+        public int Energetico { get; set; }
+
+        public int Agua { get; set; }
+
+        public double ValorTotal { get; set; }
+
+        public void Comprar()
+        {
+            if(OpcaoMenu == "1" && Refrigerante > 0)
+            {
+                Refrigerante = 0;
+                ValorTotal = ValorTotal + 5.00;
+                Console.WriteLine("Produto entregue!");
+            }
+            else if (OpcaoMenu == "1" && Refrigerante == 0)
+            {
+                Console.WriteLine("O produto precisa ser reposto");
+            }
+            if (OpcaoMenu == "2" && Energetico > 0)
+            {
+                Energetico = 0;
+                ValorTotal = ValorTotal + 10.00;
+                Console.WriteLine("Produto entregue!");
+            }
+            else if (OpcaoMenu == "2" && Energetico == 0)
+            {
+                Console.WriteLine("O produto precisa ser reposto");
+            }
+            if (OpcaoMenu == "3" && Agua > 0)
+            {
+                Agua = 0;
+                ValorTotal = ValorTotal + 2.00;
+                Console.WriteLine("Produto entregue!");
+            }
+            else if (OpcaoMenu == "3" && Agua == 0)
+            {
+                Console.WriteLine("O produto precisa ser reposto");
+            }
+            if (OpcaoMenu == "4" && Suco > 0)
+            {
+                Suco = 0;
+                ValorTotal = ValorTotal + 5.00;
+                Console.WriteLine("Produto entregue!");
+            }
+            else if (OpcaoMenu == "4" && Suco == 0)
+            {
+                Console.WriteLine("O produto precisa ser reposto");
+            }
+            if (OpcaoMenu == "5" && Cafe > 0)
+            {
+                Cafe = 0;
+                ValorTotal = ValorTotal + 2.50;
+                Console.WriteLine("Produto entregue!");
+            }
+            else if (OpcaoMenu == "5" && Cafe == 0)
+            {
+                Console.WriteLine("O produto precisa ser reposto");
+            }
+        }
+        public void Repor()
+        {
+            if (OpcaoMenu == "r")
+            {
+                Console.WriteLine("Digite o número do produto que deseja repor");
+                string produtoR = Console.ReadLine();
+
+                if (produtoR == "1" && Refrigerante == 0)
+                {
+                    Refrigerante++;
+                    Console.WriteLine("O produto adicionado ao estoque");
+                }
+                else if (produtoR == "1" && Refrigerante > 0)
+                {
+                    Console.WriteLine("Produto já está no estoque");
+                }
+                if (produtoR == "2" && Energetico == 0)
+                {
+                    Energetico++;
+                    Console.WriteLine("O produto adicionado ao estoque");
+                }
+                else if (produtoR == "2" && Energetico > 0)
+                {
+                    Console.WriteLine("Produto já está no estoque");
+                }
+                if (produtoR == "3")
+                {
+                    Agua++;
+                    Console.WriteLine("O produto adicionado ao estoque");
+                }
+                else if (produtoR == "3" && Agua > 0)
+                {
+                    Console.WriteLine("Produto já está no estoque");
+                }
+                if (produtoR == "4")
+                {
+                    Suco++;
+                    Console.WriteLine("O produto adicionado ao estoque");
+                }
+                else if (produtoR == "4" && Suco > 0)
+                {
+                    Console.WriteLine("Produto já está no estoque");
+                }
+                if (produtoR == "5")
+                {
+                    Cafe++;
+                    Console.WriteLine("O produto adicionado ao estoque");
+                }
+                else if (produtoR == "5" && Cafe > 0)
+                {
+                    Console.WriteLine("Produto já está no estoque");
+                }
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using MaquinaDeBebidas;
+
+Maquina maquina = new Maquina();
+
+maquina.Refrigerante = 1;
+maquina.Energetico = 1;
+maquina.Agua = 1;
+maquina.Suco = 1;
+maquina.Cafe = 1;
+bool loop = true;
+
+while (loop)
+{
+    Console.WriteLine("Escolha uma das opções do menu:");
+    Console.Write("1 - refrigerante R$:5,00 -- 2 - energético R$:10,00 -- 3 - água R$: 2,00 -- 4 - suco R$:5,00 -- 5 - café R$:2,50 -- (R) - Repor -- (S) - Sair: ");
+    maquina.OpcaoMenu = Console.ReadLine();
+
+    if (maquina.OpcaoMenu == "1")
+    {
+        Console.Clear();
+        maquina.Comprar();
+    }
+    if (maquina.OpcaoMenu == "2")
+    {
+        Console.Clear();
+        maquina.Comprar();
+    }
+    if (maquina.OpcaoMenu == "3")
+    {
+        Console.Clear();
+        maquina.Comprar();
+    }
+    if (maquina.OpcaoMenu == "4")
+    {
+        Console.Clear();
+        maquina.Comprar();
+    }
+    if (maquina.OpcaoMenu == "5")
+    {
+        Console.Clear();
+        maquina.Comprar();
+    }
+    if (maquina.OpcaoMenu == "r")
+    {
+        Console.Clear();
+        maquina.Repor();
+    }
+    if (maquina.OpcaoMenu == "s")
+    {
+        Console.Clear();
+        Console.WriteLine($"Obrigado por usar nossa máquina. Seu total gasto foi de: {maquina.ValorTotal}");
+        break;
+
+    }
+}
